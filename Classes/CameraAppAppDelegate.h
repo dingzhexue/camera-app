4093830bb51eee23f1a640a15d9cfa06053f5bae
System Administrator <root@bogon.(none)>
2012-02-02 10:35:58 +0800

    init

---
+++ Classes/CameraAppAppDelegate.h
@@ -0,0 +1,18 @@
+//
+//  CameraAppAppDelegate.h
+//  CameraApp
+//
+//  Created by System Administrator on 2/2/12.
+//  Copyright 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CameraAppAppDelegate : NSObject <UIApplicationDelegate> {
+    UIWindow *window;
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
+
